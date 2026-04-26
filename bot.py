eda2bab36c233764c0f8f0e8d86e7531facf7382
Taddie Mason <137760738+taddiemason@users.noreply.github.com>
2026-04-26 19:49:53 -0400

    Fix trivia win tracking KeyError

--- bot.py
+++ bot.py
@@ -1156,7 +1156,8 @@ def spend_coins(user_id, amount):
 def record_trivia_win(user_id):
     eco = load_economy()
     uid = str(user_id)
-    eco.setdefault("trivia_wins", {})[uid] = eco["trivia_wins"].get(uid, 0) + 1
+    trivia_wins = eco.setdefault("trivia_wins", {})
+    trivia_wins[uid] = trivia_wins.get(uid, 0) + 1
     save_economy(eco)
 
 
